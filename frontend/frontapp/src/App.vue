31e5e30ec551e778faec470c12db57707c3a5c44
UK-44 <cos.of.you.winds@gmail.com>
2021-09-15 22:46:58 +0900

    router 整備

--- frontend/frontapp/src/App.vue
+++ frontend/frontapp/src/App.vue
@@ -1,25 +1,39 @@
 <template>
-  <HelloWorld />
+  <div id="app">
+    <Header />
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Header from "./components/globals/Header.vue";
 export default {
-  name: "App",
   components: {
-    HelloWorld,
+    Header,
   },
 };
 </script>
 
 <style>
+@import "./style/normalize.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ frontend/frontapp/src/components/atoms/AppLogo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app_logo">
+    <p class="app_logo_title">logo</p>
+  </div>
+</template>>
+
+<style lang="scss" scoped>
+.app_logo {
+  &_title {
+    font-size: 100px;
+    color: #fff;
+  }
+}
+</style>
